ccf98c09ea90ed0645b5e92ce8f6375be27c788f
Bruno Ferreira <bruno_ferreira@hotmail.it>
2022-06-27 13:53:16 +0200

    VB.Net Project added

---
+++ Collection.Lesson.VB/Program.vb
@@ -0,0 +1,33 @@
+Imports System.Collections.Generic
+Module Program
+    Sub Main()
+        Dim Lt As List(Of Integer) = New List(Of Integer)()
+        Dim i As Integer
+        'Dim j As String  
+        Console.WriteLine(" Added elements in List1")
+        ' Use of Add() function  
+        Lt.Add(50)
+        Lt.Add(55)
+        Lt.Add(10)
+        'Lt.Add(70)  
+
+        Dim Lt2 As List(Of String) = New List(Of String)()
+        Lt2.Add("Welcome")
+        Lt2.Add("to")
+        Lt2.Add("JavaTpoint")
+        Lt2.Add("Site")
+
+        'Access all element of List1  
+        For Each i In Lt
+            Console.WriteLine("{0}", i)
+        Next i
+        Console.WriteLine("Total elements in list1: {0}", Lt.Count)
+        Console.WriteLine("Number of elements can be placed in list1: {0}", Lt.Capacity)
+        Console.WriteLine()
+
+        'Access all element of list2  
+        Console.WriteLine(" Added elements in List2")
+
+
+    End Sub
+End Module
